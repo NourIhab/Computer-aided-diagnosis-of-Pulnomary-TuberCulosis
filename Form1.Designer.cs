7abfcb48e4355edc8b99b10d61fb89445284ad10
NourIhab <45313564+NourIhab@users.noreply.github.com>
2019-07-26 19:44:43 +0200

    GUI

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Upload_Img = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.test = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Upload_Img
+            // 
+            this.Upload_Img.Location = new System.Drawing.Point(38, 317);
+            this.Upload_Img.Name = "Upload_Img";
+            this.Upload_Img.Size = new System.Drawing.Size(87, 40);
+            this.Upload_Img.TabIndex = 3;
+            this.Upload_Img.Text = "Browse Image";
+            this.Upload_Img.UseVisualStyleBackColor = true;
+            this.Upload_Img.Click += new System.EventHandler(this.Upload_Img_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(38, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(394, 296);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "Upload Image (user 1)";
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(38, 363);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(87, 40);
+            this.test.TabIndex = 4;
+            this.test.Text = "Test Image";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.test_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox1.Location = new System.Drawing.Point(241, 363);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 33);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(238, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "PULMONARY TUBERCULOSIS Result";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(485, 418);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.Upload_Img);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "PULMONARY TUBERCULOSIS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Upload_Img;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button test;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(@"background.png");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private string python_run(string python_code)
+        {
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"env.txt");
+            string line = file.ReadLine();
+            line.Replace("\\", "\\\\");
+            
+            Process p = new Process();
+
+            p.StartInfo = new ProcessStartInfo(@line,python_code )
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+            p.Start();
+            p.WaitForExit();
+
+            string output = p.StandardOutput.ReadToEnd();
+            // string stderr = p.StandardError.ReadToEnd();
+            // textBox1.Text = stderr;
+            p.Close();
+            return output;
+        }
+        
+        private void Upload_Img_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            OpenFileDialog opnfd = new OpenFileDialog();
+            opnfd.Filter = "Image Files (*.png;*.jpg;*.jpeg;.*.gif;)|*.png;*.jpg;*.jpeg;.*.gif";
+            if (opnfd.ShowDialog() == DialogResult.OK)
+            {
+                string ImgDir = opnfd.FileName;
+                Bitmap img = (Bitmap)Image.FromFile(ImgDir, true);
+                img.Save(@"image.jpg");
+                pictureBox1.Image = new Bitmap(opnfd.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            
+            }  
+        }
+
+        private void test_Click(object sender, EventArgs e)
+        {
+            string result = python_run("main_model.py image.jpg");
+            textBox1.Text = result;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "Normal")
+            {
+                textBox1.ForeColor = Color.Green;
+            }
+            else
+            {
+                textBox1.ForeColor = Color.Red;
+
+            }
+        }
+    }
+}
